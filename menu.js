af6b356d01b2c2d0dacf0154748747095a5aa16f
Thomas Blumstein <61987113+Oute-Oute@users.noreply.github.com>
2022-12-02 07:01:21 +0100

    konami

--- menu.js
+++ menu.js
@@ -14,3 +14,29 @@ function openNav() {
 function closeNav() {
   sidenav.classList.remove("active");
 }
+
+
+var pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+var current = 0;
+
+var keyHandler = function (event) {
+
+	// If the key isn't in the pattern, or isn't the current key in the pattern, reset
+	if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+		current = 0;
+		return;
+	}
+
+	// Update how much of the pattern is complete
+	current++;
+
+	// If complete, alert and reset
+	if (pattern.length === current) {
+		current = 0;
+    document.getElementById("konami").hidden=false;
+	}
+
+};
+
+// Listen for keydown events
+document.addEventListener('keydown', keyHandler, false);
