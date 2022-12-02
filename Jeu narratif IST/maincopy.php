c5f1f1f9ad4dc760a0d3a986e9e71389b4a721da
Thomas Blumstein <61987113+Oute-Oute@users.noreply.github.com>
2022-12-02 01:06:09 +0100

    prepare website

--- Jeu narratif IST/maincopy.php
+++ Jeu narratif IST/maincopy.php
@@ -3,6 +3,7 @@
     <head>
         <title></title>
         <meta charset="utf-8">
+        <script src="questions.js"></script>
     </head>
     <body>
         <input hidden id="data" value="test">
@@ -11,12 +12,19 @@
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam earum maxime aperiam in eligendi praesentium explicabo repellat delectus dolorem nulla! Totam cum quidem rerum quas rem repudiandae necessitatibus quis! Numquam?
             </p>
         </div>
-        <button id="reponseA" onclick="repondre(0)"></button>
-        <button id="reponseB" onclick="repondre(1)"></button>
-        <button id="reponseC" onclick="repondre(2)"></button>
-        <br>
-        <button hidden id="suivant" onclick="suivantClick()">Suivant</button>
+        <div id="réponse1">
+            <p>
+            </p>
+        </div>
+        <div id="réponse2">
+            <p>
+                Réponse B
+            </p>
+        </div>
+        <div id="réponse3">
+            <p>
+                Réponse B
+            </p>
+        </div>
     </body>
 </html>
-
-<script src="questions.js"></script>
