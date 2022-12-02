361d3c60da8d2b9b71152a490e6328f80faf81c3
Thomas Blumstein <61987113+Oute-Oute@users.noreply.github.com>
2022-12-02 03:29:49 +0100

    ouais ouais commit

--- Jeu narratif IST/contact.php
+++ Jeu narratif IST/contact.php
@@ -4,5 +4,22 @@
         <title></title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="main_css.css">
+        <link rel="stylesheet" href="marco.css"/>
         <?php include("menu.html"); ?>
     </head>
+    <body>
+    <h1>Si vous souhaitez recevoir plus d’informations vous pouvez toujours visiter ces sites : </h1>
+
+    <div id="main">
+    <div id="content">
+        <a href="https://www.sida-info-service.org"><img src=""></a>
+        <a href="https://www.sexualites-info-sante.fr"><img src=""></a>
+        <a href="https://www.hepatites-info-service.org"><img src=""></a>
+        <a href="https://www.vih-info-soignants.fr"><img src=""></a>
+        <a href="https://www.ligneazur.org"><img src=""></a>
+        <a href="https://www.instagram.com/sida_info_service"><img src=""></a>
+        <a href="https://www.instagram.com/sexualites_info_sante"><img src=""></a>
+        <a href="https://www.facebook.com/SidaInfoService"><img src=""></a>
+
+
+    </body>
