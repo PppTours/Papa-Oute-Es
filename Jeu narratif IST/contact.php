4d6f2ea585ba11ec9e2cf3fe0748f69039b2f032
Thomas Blumstein <61987113+Oute-Oute@users.noreply.github.com>
2022-12-02 04:12:10 +0100

    commit de 4h12

--- Jeu narratif IST/contact.php
+++ Jeu narratif IST/contact.php
@@ -11,15 +11,17 @@
     <h1>Si vous souhaitez recevoir plus d’informations vous pouvez toujours visiter ces sites : </h1>
 
     <div id="main">
-    <div id="content">
-        <a href="https://www.sida-info-service.org"><img src=""></a>
-        <a href="https://www.sexualites-info-sante.fr"><img src=""></a>
-        <a href="https://www.hepatites-info-service.org"><img src=""></a>
-        <a href="https://www.vih-info-soignants.fr"><img src=""></a>
-        <a href="https://www.ligneazur.org"><img src=""></a>
-        <a href="https://www.instagram.com/sida_info_service"><img src=""></a>
-        <a href="https://www.instagram.com/sexualites_info_sante"><img src=""></a>
-        <a href="https://www.facebook.com/SidaInfoService"><img src=""></a>
-
+    <div class="contentContact">
+        <a class="contactPic" href="https://www.sida-info-service.org"><img src="img/Sida info service.png"></a>
+        <a class="contactPic" href="https://www.sexualites-info-sante.fr"><img src="img/sexualités info santé.png"></a>
+        <a class="contactPic" href="https://www.hepatites-info-service.org"><img src="img/hepatites info service.png"></a>
+        <a class="contactPic" href="https://www.vih-info-soignants.fr"><img src="img/VIH info soignants.png"></a>
+        <br>
+        <a class="contactPic" href="https://www.ligneazur.org"><img src="img/ligne azur .png"></a>
+        <a class="contactPic" href="https://www.instagram.com/sida_info_service"><img src="img/instagram sida-info service.png"></a>
+        <a class="contactPic" href="https://www.instagram.com/sexualites_info_sante"><img src="img/instagram sexualite info-sante.png"></a>
+        <a class="contactPic" href="https://www.facebook.com/SidaInfoService"><img src="img/facebook sidainfoservice.png"></a>
+    </div>
+    </div>
 
     </body>
